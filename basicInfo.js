252ba8d0087525cfea3bc6964188afb7c73d1276
LUCAS <0910lmadrid4f@gmail.com>
2024-05-28 18:37:06 +0200

    render

--- basicInfo.js
+++ basicInfo.js
@@ -1,211 +1,162 @@
+const paths = require('path');
 
 const swaggerOptions = {
-    "openapi": "3.0.0",
-    "info": {
-        "title": "OpenAPI Bug Test",
-        "description": "OpenAPI specification Test in GitLab",
-        "contact": {
-            "name": "Test",
-        "url": "http://localhost:8000",
-    
-            "email": "test@domain.tld"
-        },
-        "license": {
-            "name": "proprietary"
-        },
-        "version": "1.0"
+  openapi: '3.0.0',
+  info: {
+    title: 'OpenAPI Bug Test',
+    description: 'OpenAPI specification Test in GitLab',
+    contact: {
+      name: 'Test',
+      url: 'http://localhost:8000',
+      email: 'test@domain.tld'
     },
-    "servers": [
-        {
-        "url": "http://localhost:8000",
-        "description": "Test Server"
-        }
-    ],
-    "paths": {
-        "/create": {
-            post: {
-              tags: {
-                Tasks: " Create a Task.",
-              },
-              description: "Create a Task",
-              operationId: "Create a Task",
-              parameters: [
-                {
-                  name: "_id",
-                  in: "path",
-                  schema: {
-                    $ref: "#/components/schemas/_id",
-                  },
-                  description: "Id of User to be updated",
-                },
-              ],
-              requestBody: {
-                content: {
-                  "application/json": {
-                    schema: { $ref: "#/components/schemas/User" },
-                  },
-                },
-              },
-              responses: {
-                200: { description: "User updated successfully" },
-                500: { description: "Server error" },
-              },
-            },
-          },
-        "/id/:_id": {
-            get: {
-              tags: {
-                Tasks: " Get  Task by ID.",
-              },
-              description: "Get  Task by ID.",
-              operationId: "Get  Task by ID.",
-              parameters: [
-                {
-                  name: "_id",
-                  in: "path",
-                  schema: {
-                    $ref: "#/components/schemas/_id",
-                  },
-                  description: "Id of User to be updated",
-                },
-              ],
-              requestBody: {
-                content: {
-                  "application/json": {
-                    schema: { $ref: "#/components/schemas/User" },
-                  },
-                },
-              },
-              responses: {
-                200: { description: "User updated successfully" },
-                500: { description: "Server error" },
-              },
-            },
-          },
-        "/": {
-            get: {
-              tags: {
-                Tasks: " Get all TASKS.",
-              },
-              description: "Get all TASKS.",
-              operationId: "Get all TASKS.",
-              parameters: [
-                {
-                  name: "_id",
-                  in: "path",
-                  schema: {
-                    $ref: "#/components/schemas/_id",
-                  },
-                  description: "Id of User to be updated",
-                },
-              ],
-              requestBody: {
-                content: {
-                  "application/json": {
-                    schema: { $ref: "#/components/schemas/User" },
-                  },
-                },
-              },
-              responses: {
-                200: { description: "User updated successfully" },
-                500: { description: "Server error" },
-              },
-            },
-          },
-
-
-    "/id/:_id": {
-        put: {
-          tags: {
-            Tasks: "Update a user",
-          },
-          description: "Update User",
-          operationId: "updateUser",
-          parameters: [
-            {
-              name: "_id",
-              in: "path",
-              schema: {
-                $ref: "#/components/schemas/_id",
-              },
-              description: "Id of User to be updated",
-            },
-          ],
-          requestBody: {
-            content: {
-              "application/json": {
-                schema: { $ref: "#/components/schemas/User" },
-              },
-            },
-          },
-          responses: {
-            200: { description: "User updated successfully" },
-            500: { description: "Server error" },
-          },
-        },
-      },
-      "/markAsCompleted/:_id": {
-        put: {
-          tags: {
-            Tasks: " Mark  as completed",
-          },
-          description: "Mark  as completed",
-          operationId: "Mark task completed",
-          parameters: [
-            {
-              name: "_id",
-              in: "path",
-              schema: {
-                $ref: "#/components/schemas/_id",
-              },
-              description: "Id of User to be updated",
-            },
-          ],
-          requestBody: {
-            content: {
-              "application/json": {
-                schema: { $ref: "#/components/schemas/User" },
-              },
-            },
-          },
-          responses: {
-            200: { description: "User updated successfully" },
-            500: { description: "Server error" },
-          },
-        },
-      },
-    
-      "/id/:id": {
-        "delete": {
-            "summary": "Delete  TASK.",
-            "description": "Delete all Tasks.",
-            "security": [],
-            "tags": [
-                "Test"
-            ],
-            "operationId": "deleteSomething",
-            "responses": {
-                "204": {
-                    "description": "Indicates, the request was successful."
-                },
-                "500": {
-                    "description": "Indicates, the request failed."
-                }
-            }
-        }
-    
-    
-},
-  
-        
-    
-            
-        
-        
+    license: {
+      name: 'proprietary'
+    },
+    version: '1.0'
+  },
+  servers: [
+    {
+      url: 'http://localhost:8000',
+      description: 'Test Server'
     }
+  ],
+  paths: {
+    '/create': {
+      post: {
+        tags: ['Tasks'],
+        summary: 'Create a Task',
+        description: 'Create a Task',
+        operationId: 'createTask',
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: { $ref: '#/components/schemas/User' }
+            }
+          }
+        },
+        responses: {
+          200: { description: 'Task created successfully' },
+          500: { description: 'Server error' }
+        }
+      }
+    },
+    '/id/{_id}': {
+      get: {
+        tags: ['Tasks'],
+        summary: 'Get Task by ID',
+        description: 'Get Task by ID',
+        operationId: 'getTaskById',
+        parameters: [
+          {
+            name: '_id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            },
+            description: 'Id of Task to be retrieved'
+          }
+        ],
+        responses: {
+          200: { description: 'Task retrieved successfully' },
+          500: { description: 'Server error' }
+        }
+      },
+      put: {
+        tags: ['Tasks'],
+        summary: 'Update a Task',
+        description: 'Update a Task',
+        operationId: 'updateTask',
+        parameters: [
+          {
+            name: '_id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            },
+            description: 'Id of Task to be updated'
+          }
+        ],
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: { $ref: '#/components/schemas/User' }
+            }
+          }
+        },
+        responses: {
+          200: { description: 'Task updated successfully' },
+          500: { description: 'Server error' }
+        }
+      },
+      delete: {
+        summary: 'Delete a Task',
+        description: 'Delete a Task',
+        tags: ['Tasks'],
+        operationId: 'deleteTask',
+        parameters: [
+          {
+            name: '_id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            },
+            description: 'Id of Task to be deleted'
+          }
+        ],
+        responses: {
+          204: { description: 'Task deleted successfully' },
+          500: { description: 'Server error' }
+        }
+      }
+    },
+    '/markAsCompleted/{_id}': {
+      put: {
+        tags: ['Tasks'],
+        summary: 'Mark Task as Completed',
+        description: 'Mark Task as Completed',
+        operationId: 'markTaskAsCompleted',
+        parameters: [
+          {
+            name: '_id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            },
+            description: 'Id of Task to be marked as completed'
+          }
+        ],
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: { $ref: '#/components/schemas/User' }
+            }
+          }
+        },
+        responses: {
+          200: { description: 'Task marked as completed successfully' },
+          500: { description: 'Server error' }
+        }
+      }
+    },
+    '/': {
+      get: {
+        tags: ['Tasks'],
+        summary: 'Get all Tasks',
+        description: 'Get all Tasks',
+        operationId: 'getAllTasks',
+        responses: {
+          200: { description: 'Tasks retrieved successfully' },
+          500: { description: 'Server error' }
+        }
+      }
+    }
+  },
 
-  
-}
-
+};
 
 module.exports = swaggerOptions;
